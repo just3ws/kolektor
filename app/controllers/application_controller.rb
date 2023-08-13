2e48b7c45fc3a719f6eaa7a921eb89e98fcfd70c
Mike Hall <mike@just3ws.com>
2023-08-13 10:55:40 -0500

    Added catch-all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
-class ApplicationController < ActionController::Base
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base # :nodoc:
+  rescue_from Exception, with: :handle_internal_server_error
+
+  private
+
+  def handle_internal_server_error(exception)
+    # Log the exception or perform any necessary actions
+    # You can also render a custom error page if desired
+    render plain: '500 Internal Server Error', status: :internal_server_error
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # get 'errors/not_found'
+  # get 'errors/internal_server_error'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Catch-all route for routing errors (404)
+
+  match '*unmatched_route', to: 'errors#not_found', via: :all
+
 end
